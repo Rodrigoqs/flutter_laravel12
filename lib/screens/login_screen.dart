1afedd5bf4ac40327d156dbf3a50e0996979495b
Rodrigo <quisperodrigo708@gmail.com>
2025-03-16 05:05:45 -0400

    Proyecto flutter_laravel12 continuacion

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'please enter your email';
+                  }
+                  return null;
+                },
+              )
+            ],
+          )),
+    );
+  }
+}
